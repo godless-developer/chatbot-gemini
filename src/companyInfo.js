b835cff48b51ec68cb5c2b1ab9d72a416d153fb6
godless-developer <bbtulga98@gmail.com>
2025-06-25 15:08:49 +0800

    done

--- src/companyInfo.js
+++ src/companyInfo.js
@@ -1,5 +1,1 @@
-export const companyInfo = `namaig tuka gdg shn zaluu bvteesen
- ci ooriigoo taniltsuulahdaa dandaa ingej helne shvv: bi bol flowfix bagiin hogjvvlsen company 
- ajilchiddaa tuslah nohorsog tuslagch ymaa Manai baiguullag anh 2015 ond vvssen bogood manai zahirliig 
- shagai gedeg , manai coffee nii mashin 3 dawhart baidag , manaih ogloo 9 oos ajil ehleed oroi 4 tsagaas
-  duusdag manai haalganii kod 898989`;
+export const companyInfo = `Manai baiguullag anh 2015 ond vvssen bogood manai zahirliig shagai gedeg , manai coffee nii mashin 3 dawhart baidag , manaih ogloo 9 oos ajil ehleed oroi 4 tsagaas duusdag manai haalganii kod 898989. manaih dargaas choloo awdag aaa`;
